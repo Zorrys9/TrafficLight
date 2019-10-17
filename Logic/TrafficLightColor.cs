a0f7477eb68a63c9f098ec7d34069bb06e0d1cd5
saschek <saschek2000@yandex.ru>
2019-10-17 10:30:37 +0300

    light

---
+++ Logic/TrafficLightColor.cs
@@ -0,0 +1,46 @@
+﻿namespace Logic
+{
+    public class TrafficLightColor
+    {
+        public static bool red = true;
+        public enum EnumColor
+        {
+            Red,
+            Yellow,
+            Green
+        }
+        public static EnumColor CurrentColor;
+
+        public static void Change(object sender, object e)
+        {
+            switch (CurrentColor)
+            {
+                case EnumColor.Yellow:
+                    if (!red)
+                    {
+                        CurrentColor = EnumColor.Red;
+                    }
+                    else if (red)
+                    {
+                        CurrentColor = EnumColor.Green;
+                    }
+
+                    break;
+                case EnumColor.Red:
+                    CurrentColor = EnumColor.Yellow;
+                    red = true;
+                    break;
+                case EnumColor.Green:
+                    CurrentColor = EnumColor.Yellow;
+                    red = false;
+                    break;
+                default:
+                    CurrentColor = EnumColor.Red;
+                    break;
+            }
+
+        }
+
+
+    }
+}
---
+++ TrafficLight/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Logic;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace TrafficLight
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        readonly DispatcherTimer timer;
+        public int count = 0;
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                TrafficLightColor.CurrentColor = TrafficLightColor.EnumColor.Red;
+                timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 5) };
+                timer.Tick += TrafficLightColor.Change;
+                timer.Tick += ChangeColor;
+                timer.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void ChangeColor(object sender, object e)
+        {
+
+            switch (TrafficLightColor.CurrentColor)
+            { 
+                case TrafficLightColor.EnumColor.Red:
+                    RedLight.Fill = Brushes.Red;
+                    GreenLight.Fill = Brushes.Black;
+                    YellowLight.Fill = Brushes.Black;
+                    break;
+                case TrafficLightColor.EnumColor.Yellow:
+                    RedLight.Fill = Brushes.Black;
+                    GreenLight.Fill = Brushes.Black;
+                    YellowLight.Fill = Brushes.Yellow;
+                    break;
+                case TrafficLightColor.EnumColor.Green:
+                    RedLight.Fill = Brushes.Black;
+                    GreenLight.Fill = new SolidColorBrush(Color.FromRgb(0,255,0));
+                    YellowLight.Fill = Brushes.Black;
+                    break;
+            }
+        }
+    }
+}
